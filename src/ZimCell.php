6ec18d697ad913b018615dca3dd653c2835948f0
zerochip <semhiles@gmail.com>
2018-02-25 20:05:09 +0200

    Doc blocks for ZimCell class and ZimCellTest class

--- src/ZimCell.php
+++ src/ZimCell.php
@@ -2,112 +2,187 @@
 
 namespace Zerochip;
 
+/**
+ * ZimCell
+ *
+ * Main Class for the ZimCell package
+ */
 class ZimCell
 {
+    /**
+     * Gets codes for a provider or service
+     *
+     * @param string $provider Provider or service name
+     *
+     * @throws Exception
+     *
+     * @return array Codes available for that provider or service
+     */
+    public function codesFor($provider)
+    {
+        $netoneCodes = ['71'];
+        $telecelCodes = ['73'];
+        $econetCodes = ['77', '78'];
 
-	public function codesFor($provider)
-	{
-		$netoneCodes = ['71'];
-		$telecelCodes = ['73'];
-		$econetCodes = ['77', '78'];
+        $codes = [
+            'econet' 	=> $econetCodes,
+            'ecocash'	=> $econetCodes,
+            'telecel'	=> $telecelCodes,
+            'telecash'	=> $telecelCodes,
+            'netone'	=> $netoneCodes,
+            'onemoney'	=> $netoneCodes
+        ];
 
-		$codes = [
-			'econet' 	=> $econetCodes,
-			'ecocash'	=> $econetCodes,
-			'telecel'	=> $telecelCodes,
-			'telecash'	=> $telecelCodes,
-			'netone'	=> $netoneCodes,
-			'onemoney'	=> $netoneCodes
-		];
+        if (!isset($codes[$provider])) {
+            throw new Exception("Unknown provider or service");
+        }
 
-		if(!isset($codes[$provider]))
-		{
-			// unknown provider
-			return;
-		}
-		
-		return $codes[$provider];
-	}
-	
-	public function refine($num)
-	{
-		// remove spacing within number
-  		$refinedNum = str_replace(' ', '', $num);
+        return $codes[$provider];
+    }
 
-  		// remove the first zero
-      	if (substr($refinedNum, 0, 1) == '0'){
-      		$refinedNum = str_replace('0', '', $refinedNum);
-      	}
+    /**
+     * Refines a given phone number
+     *
+     * Spaces, country code(+263, 263), and the leading zero is removed
+     *
+     * @param string $num Cellnumber
+     *
+     * @return string The refined phone number
+     */
+    public function refine($num)
+    {
+        // remove spacing within number
+        $refinedNum = str_replace(' ', '', $num);
 
-      	$f3d = substr($refinedNum, 0, 3);
-      	$f4d = substr($refinedNum, 0, 4);
+        // remove the first zero
+        if (substr($refinedNum, 0, 1) == '0') {
+            $refinedNum = str_replace('0', '', $refinedNum);
+        }
 
-      	// first 3 digits
-      	if ($f3d == '263') {
-      		$refinedNum = substr($refinedNum, 3);
-      	}
+        $f3d = substr($refinedNum, 0, 3);
+        $f4d = substr($refinedNum, 0, 4);
 
-      	// first 4 digits
-      	if ($f4d == '+263') {
-      		$refinedNum = substr($refinedNum, 4);
-      	}
+        // first 3 digits
+        if ($f3d == '263') {
+            $refinedNum = substr($refinedNum, 3);
+        }
 
-      	return $refinedNum;
-	}
+        // first 4 digits
+        if ($f4d == '+263') {
+            $refinedNum = substr($refinedNum, 4);
+        }
 
-	public function valid($num)
-	{
-		$refinedNum = self::refine($num);
-		$firstDigit7 = substr($refinedNum, 0) == '7';
+        return $refinedNum;
+    }
 
-		if (!is_numeric($refinedNum) && strlen($refinedNum) != 9 && !$firstDigit7)
-		{
-			return false;
-		}
+    /**
+     * Verifys if the cellnumber is valid for Zimbabwe
+     *
+     * @param string $num Cellnumber
+     *
+     * @return string The refined phone number
+     */
+    public function valid($num)
+    {
+        $refinedNum = self::refine($num);
+        $firstDigit7 = substr($refinedNum, 0) == '7';
 
-		return true;
-	}
+        if (!is_numeric($refinedNum) && strlen($refinedNum) != 9 && !$firstDigit7) {
+            return false;
+        }
 
-	public function is($provider, $num)
-	{
-		$f2d = substr(self::refine($num), 0, 2);
-		$codes = self::codesFor($provider);
+        return true;
+    }
 
-		if(!in_array($f2d, $codes))
-		{
-			return false;
-		}
+    /**
+    * Verifys if a number is valid for a provider or service
+    *
+    * @param string $provider Provider or service name
+    * @param string $num Cellnumber
+    *
+    * @return boolean
+    */
+    public function is($provider, $num)
+    {
+        $f2d = substr(self::refine($num), 0, 2);
+        $codes = self::codesFor($provider);
 
-		return true;
-	}
+        if (!in_array($f2d, $codes)) {
+            return false;
+        }
 
-	public function isEconet($num)
-	{
-		return self::is('econet', $num);
-	}
+        return true;
+    }
 
-	public function isEcocash($num)
-	{
-		return self::is('ecocash', $num);
-	}
+    /**
+     * Verifys if a cellnumber is a valid for econet
+     *
+     * @param string $num Cellnumber
+     *
+     * @return boolean
+     */
+    public function isEconet($num)
+    {
+        return self::is('econet', $num);
+    }
 
-	public function isTelecel($num)
-	{
-		return self::is('telecel', $num);
-	}
+    /**
+     * Verifys if a cellnumber is a valid for ecocash
+     *
+     * @param string $num Cellnumber
+     *
+     * @return boolean
+     */
+    public function isEcocash($num)
+    {
+        return self::is('ecocash', $num);
+    }
 
-	public function isTelecash($num)
-	{
-		return self::is('telecel', $num);
-	}
+    /**
+     * Verifys if a cellnumber is a valid for telecel
+     *
+     * @param string $num Cellnumber
+     *
+     * @return boolean
+     */
+    public function isTelecel($num)
+    {
+        return self::is('telecel', $num);
+    }
 
-	public function isNetone($num)
-	{
-		return self::is('netone', $num);
-	}
+    /**
+     * Verifys if a cellnumber is a valid for telecash
+     *
+     * @param string $num Cellnumber
+     *
+     * @return boolean
+     */
+    public function isTelecash($num)
+    {
+        return self::is('telecel', $num);
+    }
 
-	public function isOneMoney($num)
-	{
-		return self::is('onemoney', $num);
-	}
+    /**
+     * Verifys if a cellnumber is a valid for netone
+     *
+     * @param string $num Cellnumber
+     *
+     * @return boolean
+     */
+    public function isNetone($num)
+    {
+        return self::is('netone', $num);
+    }
+
+    /**
+     * Verifys if a cellnumber is a valid for onemoney
+     *
+     * @param string $num Cellnumber
+     *
+     * @return boolean
+     */
+    public function isOneMoney($num)
+    {
+        return self::is('onemoney', $num);
+    }
 }
--- tests/ZimCellTest.php
+++ tests/ZimCellTest.php
@@ -2,63 +2,66 @@
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * ZimCellTest
+ *
+ * Tests for zimcell class method
+ */
 class ZimCellTest extends TestCase
 {
-	public function testRemoveLeadingZero()
+    public function testRemoveLeadingZero()
     {
-    	$num = Zerochip\ZimCell::refine('077123456');
+        $num = Zerochip\ZimCell::refine('077123456');
 
         $this->assertEquals('77123456', $num);
     }
 
     public function testRemoveSpaces()
     {
-    	$num = Zerochip\ZimCell::refine('077 123 4 5 6');
+        $num = Zerochip\ZimCell::refine('077 123 4 5 6');
 
         $this->assertEquals('77123456', $num);
     }
 
     public function testRemoveCountryCode1()
     {
-    	$num = Zerochip\ZimCell::refine('26377123456');
+        $num = Zerochip\ZimCell::refine('26377123456');
 
         $this->assertEquals('77123456', $num);
     }
 
     public function testRemoveCountryCode2()
     {
-    	$num = Zerochip\ZimCell::refine('+26377123456');
+        $num = Zerochip\ZimCell::refine('+26377123456');
 
         $this->assertEquals('77123456', $num);
     }
 
     public function testValid()
     {
-    	$result = Zerochip\ZimCell::valid('+26377123456');
+        $result = Zerochip\ZimCell::valid('+26377123456');
 
         $this->assertEquals($result, true);
     }
 
     public function testIsNetone()
     {
-    	$result = Zerochip\ZimCell::isNetone('71123456');
+        $result = Zerochip\ZimCell::isNetone('71123456');
 
         $this->assertEquals($result, true);
     }
 
     public function testIsTelecel()
     {
-    	$result = Zerochip\ZimCell::isTelecel('73123456');
+        $result = Zerochip\ZimCell::isTelecel('73123456');
 
         $this->assertEquals($result, true);
     }
 
     public function testIsEconet()
     {
-    	$result = Zerochip\ZimCell::isEconet('77123456');
+        $result = Zerochip\ZimCell::isEconet('77123456');
 
         $this->assertEquals($result, true);
     }
-
-
 }
